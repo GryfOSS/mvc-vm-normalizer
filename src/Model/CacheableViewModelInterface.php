27aa7c1d60f38ff8ade5f6a1e8977563ab63f3b6
IDCT Bartosz Pachołek <bartosz+github@idct.tech>
2023-11-03 12:33:40 +0100

    default view model normalizer

--- src/Model/CacheableViewModelInterface.php
+++ src/Model/CacheableViewModelInterface.php
@@ -2,8 +2,9 @@
 
 namespace Praetorian\Mvc\Model;
 use Symfony\Component\Serializer\Annotation\Ignore;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
-interface CacheableViewModelInterface
+interface CacheableViewModelInterface extends NormalizerInterface
 {
     public function getCacheKey(): string;
 }
